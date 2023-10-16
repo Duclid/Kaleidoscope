ee323e1e0ba7d4334c588ab353725efd9666cd44
duclid <401758229@qq.com>
2023-10-16 23:59:09 +0800

    :tada: Add AST Node :D

--- kaleidoscope.cpp
+++ kaleidoscope.cpp
@@ -1,10 +1,25 @@
+/**********************************
+*        ____  _  _  ____ 
+*       (    \/ )( \(  _ \
+*        ) D () __ ( ) __/
+*       (____/\_)(_/(__)  
+*
+**********************************/
+
 #include <iostream>
 #include <cctype>
 #include <cstdio>
+#include <memory>
+#include <vector>
 #include <ctype.h>
 
 #include "token.h"
 using namespace std;
+
+// ====================//
+//      词法分析器      //
+// ====================//
+
 static int gettok(){
     
     static int LastChar = ' ';
@@ -61,8 +76,87 @@ static int gettok(){
 }
 
 
+// ====================//
+//      语法分析器      //
+// ====================//
+
+//////////////////////////////// 表达式 ////////////////////////////////
+class ExprAST{
+public:
+    virtual ~ExprAST() = default;
+};
+
+class NumberExprAST : public ExprAST{
+public:
+    NumberExprAST(double val)
+        : Val(val) {}
+private:
+    double Val;
+};
+
+class VariableExprAST : public ExprAST{
+public:
+    VariableExprAST(string name)
+        : Name(name) {}
+private:
+    string Name;
+};
+
+class BinaryExprAST : public ExprAST{
+public:
+    BinaryExprAST(char op, unique_ptr<ExprAST> lhs, unique_ptr<ExprAST> rhs)
+        : Op(op), Lhs(move(lhs)), Rhs(move(rhs)) {}
+private:
+    char Op;
+    unique_ptr<ExprAST> Lhs, Rhs;
+};
+
+class CallExprAST : public ExprAST{
+public:
+    CallExprAST(string callee, vector<unique_ptr<ExprAST>> args)
+        : Callee(callee), Args(move(args)) {}
+private:
+    string Callee;
+    vector<unique_ptr<ExprAST>> Args;
+};
+
+//////////////////////////////// 函数 ////////////////////////////////
+
+/**
+ * @brief 原型用于表示一个函数的原型，用于捕获函数的名称、各个函数的参数等
+ * 
+ */
+class PrototypeAST{
+public:
+    PrototypeAST(string name, vector<unique_ptr<ExprAST>> args)
+        : Name(name), Args(move(args)){}
+private:
+    string Name;
+    vector<unique_ptr<ExprAST>> Args;
+};
+
+class FunctionAST{
+public:
+    FunctionAST(unique_ptr<PrototypeAST> proto, unique_ptr<ExprAST> body)
+        : Proto(move(proto)), Body(move(body)) {}
+private:
+    unique_ptr<PrototypeAST> Proto;
+    unique_ptr<ExprAST> Body; 
+};
+
+
+//////////////////////////////// Main ////////////////////////////////
+void PrintLogo(){
+    std::cout << " ____  _  _  ____ " << std::endl;
+    std::cout << "(    \\/ )( \\(  _ \\" << std::endl;
+    std::cout << " ) D () __ ( ) __/" << std::endl;
+    std::cout << "(____/\\_)(_/(__)" << std::endl;
+    std::cout << std::endl;
+}
 
 int main(){
+    PrintLogo();
+    // test token
     cout << gettok() << endl;
     return 0;
 }
